a7d293cf6705629ce14e677915ff7b44a9c15444
Alexi Carey <ciegoproductions@gmail.com>
2017-01-31 23:14:29 -0700

    Add fixes to submit to include submit_button, add value to comment fields so form completes, remove commented out code

--- mechanize-mike-isp.rb
+++ mechanize-mike-isp.rb
@@ -34,37 +34,18 @@ home_page = login_form.submit
 	date_form = isp_choose.form
 	date_field = date_form.field_with(name: 'ispData.dataCollectionDate')
 	date_field.value = date_arg #'01/08/2016'
-	isp_page = date_form.submit
-
+	date_button = date_form.button_with(value: 'Submit')
+	isp_page = date_form.submit date_button
 	isp_form = isp_page.form
-	#begin_field = isp_form.field_with(name: 'frm.beginTimeHour')
-	#end_field = isp_form.field_with(name: 'frm.endTimeHour')
-	#begin_min_field = isp_form.field_with(name: 'frm.beginTimeMinute')
-	#end_min_field = isp_form.field_with(name: 'frm.endTimeMinute')
 
-	#begin_am_field = isp_form.radiobuttons_with(name: 'frm.beginTimeAmPm')[0].check
-	#end_am_field = isp_form.radiobuttons_with(name: 'frm.endTimeAmPm')[1].check
 	location_field = isp_form.field_with(name: 'ispData.location')
-	#task1_field = isp_form.field_with(name: 'frm.taskScores[0].score').options[(rand(5..6))].select
-	#task2_field = isp_form.field_with(name: 'frm.taskScores[1].score').options[(rand(5..6))].select
-	#task3_field = isp_form.field_with(name: 'frm.taskScores[2].score').options[(rand(5..6))].select
-	#billable_field = isp_form.radiobuttons_with(name: 'frm.billable')[0].check
 	taskcomment1_field = isp_form.field_with(name: 'ispData.taskScores[0].comments')
 	taskcomment2_field = isp_form.field_with(name: 'ispData.taskScores[1].comments')
-	#taskcomment3_field = isp_form.field_with(name: 'ispData.comments')
-	press_field = isp_form.field_with(name: 'saveButton')
+	submit_button = isp_form.button_with(name: 'saveButton')
 
 	comment_field = isp_form.field_with(name: 'ispData.comments')
 
-	#begin_field.options[8].select
-	#end_field.options[9].select
-	#begin_min_field.options[1].select
-	#end_min_field.options[1].select
 	location_field.value = "House"
-	#taskcomment1_field.value="Mike vacuumed his room."
-	#taskcomment2_field.value="Mike took out trash."
-	#taskcomment3_field.value="na"
-	#press_field.value = "Save"
 
 	file = File.open("mikes-routine.txt")
 	contents = file.read
@@ -72,25 +53,25 @@ home_page = login_form.submit
 	file2 = File.open("Activities1.txt")
 	contents2 = file2.read
 
-	file3 = File.open("Activities2")
+	file3 = File.open("Activities2.txt")
 	contents3 = file3.read
 
 	options = contents.split("//")
 	length = options.count
 	comment = options[rand(0..(length-1))]
-  comment_field = comment #"Morning routine. Mike hung out around the house and ate lunch at home. watched some tv, and then out to dinner with HHP. Back home for a cleanup."
+        comment_field.value = comment #"Morning routine. Mike hung out around the house and ate lunch at home. watched some tv, and then out to dinner with HHP. Back home for a cleanup."
 
 	options2 = contents2.split("//")
 	length2 = options2.count
 	comment2 = options2[rand(0..(length2-1))]
-	taskcomment1_field = comment2
+	taskcomment1_field.value = comment2
 
 	options3 = contents3.split("//")
-	length3 = options.count
+	length3 = options3.count
 	comment3 = options3[rand(0..(length3-1))]
-	taskcomment2_field = comment3
+	taskcomment2_field.value = comment3
+	submitpage = isp_form.submit submit_button
 
-	submitpage = isp_form.submit
 	p "#{date_arg} processed"
         #sleep 10
 #end
